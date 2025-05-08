65d1a96469fb7eed0c56a29377c2888cac134dbf
Anna Wszeborowska <a.wszeborowska@arts.ac.uk>
2025-05-08 17:47:51 +0100

    Fix the forms and the filtering of moderated content

--- genmodels/forms.py
+++ genmodels/forms.py
@@ -1,7 +1,28 @@
 from django.forms import ModelForm
+from moderation.forms import BaseModeratedObjectForm
 from .models import MLModel
 
-class MLModelForm(ModelForm):
+class MLModelForm(BaseModeratedObjectForm):
+# class MLModelForm(ModelForm):
     class Meta:
         model = MLModel
-        fields = ["name", "description", "year"]
+        fields = [
+            "identifier",
+            "name",
+            "description",
+            "year",
+            "website",
+            "dataset",
+            "license_type",
+            "is_free",
+            "is_open_source",
+            "has_real_time_inference",
+            "are_checkpoints_available",
+            "can_finetune",
+            "can_train_from_scratch",
+            "interactions",
+            "technology",
+            "input_types",
+            "output_types",
+            "tags",
+        ]
--- genmodels/moderators.py
+++ genmodels/moderators.py
@@ -1,12 +1,19 @@
-from moderation.constants import MODERATION_STATUS_APPROVED
+from django.db.models import Count, Q
+from moderation.constants import MODERATION_READY_STATE
 from moderation.managers import ModerationObjectsManager
-from moderation.models import STATUS_CHOICES
 from moderation.moderator import GenericModerator
 
 class MultipleModerationObjectsManager(ModerationObjectsManager):
     def filter_moderated_objects(self, queryset):
-        _, approved = STATUS_CHOICES[MODERATION_STATUS_APPROVED]
-        return [q for q in queryset if q.moderated_status == approved]
+        annotated_queryset = queryset.annotate(
+            num_moderation_objects=Count('_relation_object')
+        )
+
+        only_ready = {
+            '_relation_object__state': MODERATION_READY_STATE,
+        }
+
+        return annotated_queryset.filter(Q(**only_ready))
 
 class MLModelModerator(GenericModerator):
     keep_history = True
