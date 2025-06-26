fb6cec23bc5ddde70eda2fea4901173d70fbeb71
Anna Wszeborowska <a.wszeborowska@arts.ac.uk>
2025-06-26 23:45:24 +0100

    Remove unused view

--- genmodels/views.py
+++ genmodels/views.py
@@ -45,11 +45,8 @@ def detail(request, model_id):
 
     model = get_object_or_404(MLModel, identifier=model_id)
     model.guide = md.convert(model.guide)
-    return render(request, "genmodels/detail.html", {"model": model})
 
-def detail_edit(request, model_id):
-    model = get_object_or_404(MLModel, pk=model_id)
-    return render(request, "genmodels/detail_edit.html", {"model": model})
+    return render(request, "genmodels/detail.html", {"model": model})
 
 def detail_edit_form(request, model_id):
     model = get_object_or_404(MLModel, identifier=model_id)
